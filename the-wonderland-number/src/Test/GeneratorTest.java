aab07fb5acfe3d5e64d65476be79f3e426258fcf
5Rabie5 <n.rabietarboush@gmail.com>
2019-04-30 11:02:02 +0200

    w11 1

---
+++ the-wonderland-number/src/Test/GeneratorTest.java
@@ -0,0 +1,34 @@
+package Test;
+
+import logic.Generator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeneratorTest {
+
+  Generator generator = new Generator();
+
+  @Test
+  void haveSameDigitsTest() {
+    List<Integer> integers = Arrays.asList(234, 324, 432);
+    Assertions.assertTrue(generator.haveSameDigits(integers));
+  }
+
+  @Test
+  void haveNotSameDigitsTest() {
+    List<Integer> integers = Arrays.asList(236, 354, 9432);
+    Assertions.assertFalse(generator.haveSameDigits(integers));
+  }
+
+  @Test
+  void sortNumberAscendingTest() {
+    int actual = 456732;
+    int expected = 234567;
+    Assertions.assertEquals(expected, generator.sortNumberAscending(actual));
+  }
+}
---
+++ the-wonderland-number/src/logic/Generator.java
@@ -0,0 +1,43 @@
+package logic;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Generator {
+
+  List<Integer> multipliedNumbers = Arrays.asList(2, 3, 4, 5, 6);
+
+  public int generate() {
+    int wonderlandNumberMultipliedBy2 = getListOfMultipliedByWonderlandNumber().get(0);
+    return wonderlandNumberMultipliedBy2 / 2;
+  }
+
+  private List<Integer> getListOfMultipliedByWonderlandNumber() {
+    return IntStream.range(100000, 999999)
+                    .mapToObj(this::getListOfMultipliedByPossibleNumber)
+                    .filter(this::haveSameDigits)
+                    .findFirst().get();
+  }
+
+  private List<Integer> getListOfMultipliedByPossibleNumber(int PossibleWonderlandNumber) {
+    return multipliedNumbers.stream().map(integer -> integer * PossibleWonderlandNumber)
+                            .collect(Collectors.toList());
+  }
+
+  public boolean haveSameDigits(List<Integer> numbers) {
+    int firstNumber = sortNumberAscending(numbers.get(0));
+    return numbers.stream()
+                  .map(this::sortNumberAscending)
+                  .allMatch(e -> e.equals(firstNumber));
+  }
+
+  public int sortNumberAscending(Integer number) {
+    String sortedNumber = Stream.of(String.valueOf(number).split(""))
+                                .sorted()
+                                .collect(Collectors.joining());
+    return Integer.valueOf(sortedNumber);
+  }
+}
