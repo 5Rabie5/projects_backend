25d865014cdeb3af0eda94b4a9c0918c4d9ae6a8
5Rabie5 <n.rabietarboush@gmail.com>
2019-04-30 12:56:36 +0200

    w11 4

--- the-wonderland-number/src/test/GeneratorTest.java
+++ the-wonderland-number/src/test/GeneratorTest.java
@@ -1,4 +1,4 @@
-package Test;
+package test;
 
 import logic.Generator;
 import org.junit.jupiter.api.Assertions;
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class GeneratorTest {
 
